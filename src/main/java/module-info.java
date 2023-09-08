8a47052e3adc652051608069c3d474a07e965f15
Kelston Burgess <121816599+KelsDRK@users.noreply.github.com>
2023-09-08 16:20:25 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module kbur.c482 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens kbur.c482 to javafx.fxml;
+    exports kbur.c482;
+}
