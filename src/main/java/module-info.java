3277a9af3368f01a9ea2dc601d940b1396091fc3
Kelston Burgess <121816599+KelsDRK@users.noreply.github.com>
2023-09-09 02:57:01 +0100

    FMXL and Controllers added. Window creation started.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module kbur.c482 {
                             
     opens kbur.c482 to javafx.fxml;
     exports kbur.c482;
+    exports kbur.c482.controller;
+    opens kbur.c482.controller to javafx.fxml;
 }
